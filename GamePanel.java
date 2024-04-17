dd00d40f7d7850fded6cf79df9ea844da0aa6de5
sn0wcr@sh <syedomair0@gmail.com>
2024-04-17 15:16:37 -0500

    make it a dynamic sizing window which re adjusts with changing window size

--- GamePanel.java
+++ GamePanel.java
@@ -1,27 +1,17 @@
 import java.awt.*;
-import java.awt.event.*;
-import javax.swing.JFrame;
+import javax.swing.*;
 
-public class GamePanel 
-{
-    JFrame frame;
+public class GamePanel extends JPanel {
+    private PizzaPanel pizzaPanel;
+    private MenuPanel menuPanel;
 
-    GamePanel()
-    {
-        frame = new JFrame("Resturant Game");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(900,650);
-        frame.setBackground(Color.BLACK);
-        frame.setVisible(true);
+    public GamePanel() {
+        pizzaPanel = new PizzaPanel();
+        menuPanel = new MenuPanel(pizzaPanel);
+
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, pizzaPanel, menuPanel);
+        splitPane.setDividerLocation(450);
+
+        add(splitPane);
     }
-
-    public static void main(String[] args)
-    {
-        GamePanel game = new GamePanel();
-		System.out.println("omair can also push");
-        
-    }
-
 }
-
-
