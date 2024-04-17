5e38c0eb705d8faf83758505246e8efb35ffb6d5
strejoarciles <strejoarciles@slu.edu>
2024-04-17 09:30:59 -0500

    testing out pls ignore

---
+++ GamePanel.java
@@ -0,0 +1,29 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JFrame;
+
+public class GamePanel 
+{
+    JFrame frame;
+
+    GamePanel()
+    {
+        frame = new JFrame("Resturant Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(900,650);
+        frame.setBackground(Color.BLACK);
+        frame.setVisible(true);
+
+         
+
+    }
+
+    public static void main(String[] args)
+    {
+        GamePanel game = new GamePanel();
+        
+    }
+
+}
+
+
